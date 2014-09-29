e533f7d6fa6fd504020b7aa0c3e85680daca4f6d
root <root@localhost>
2014-09-29 06:39:16 -0700

    commit to local, need some modifies

--- src/api/handlers/container.py
+++ src/api/handlers/container.py
@@ -15,40 +15,49 @@ class ContainerHandler(APIHandler):
     
     def post(self):
         args = self.get_all_arguments()
-        logging.info('all_arguments: %s' % str(args))
+        action = args.pop('action')
+        logging.info('action: %s' % action)
+        if action == 'create':
+            create_failed_rst = self.containerOpers.issue_create_action(args)
+            if create_failed_rst:
+                logging.error('container %s create failed' % create_failed_rst)
+                raise HTTPAPIError(status_code=417, error_detail="container created failed!",\
+                                    notification = "direct", \
+                                    log_message= "container created failed!",\
+                                    response =  "container created failed!")
+            dict = {}
+            dict.setdefault("message", "Success Create Container")
+            self.finish(dict)
+        
+        elif action == 'destory':
+            remove_rst = self.containerOpers.destory(args)
+            if not remove_rst:
+                raise HTTPAPIError(status_code=417, error_detail="container remove failed!",\
+                                    notification = "direct", \
+                                    log_message= "container remove failed!",\
+                                    response =  "container remove failed!")
+            dict = {}
+            dict.setdefault("message", "remove container has been done but need some time, please wait a little and check the result!")
+            self.finish(dict)         
 
-        create_failed_rst = self.containerOpers.issue_create_action(args)
-        
-        if create_failed_rst:
-            logging.error('container %s create failed' % create_failed_rst)
-            raise HTTPAPIError(status_code=417, error_detail="container created failed!",\
-                                notification = "direct", \
-                                log_message= "container created failed!",\
-                                response =  "container created failed!")
-        
-        dict = {}
-        dict.setdefault("message", "Success Create Container")
-        
-        self.finish(dict)
 
-
-@require_basic_auth
-class RemoveContainerHandler(APIHandler):
-    
-    containerOpers = Container_Opers()
-    
-    def post(self):
-        args = self.get_all_arguments()
-        logging.info('all_arguments: %s' % str(args))
-        
-        remove_rst = self.containerOpers.destory(args)
-        if not remove_rst:
-            raise HTTPAPIError(status_code=417, error_detail="container remove failed!",\
-                                notification = "direct", \
-                                log_message= "container remove failed!",\
-                                response =  "container remove failed!")
-        
-        dict = {}
-        dict.setdefault("message", "remove container has been done but need some time, please wait a little and check the result!")
-        
-        self.finish(dict)
+# @require_basic_auth
+# class RemoveContainerHandler(APIHandler):
+#     
+#     containerOpers = Container_Opers()
+#     
+#     def post(self):
+#         args = self.get_all_arguments()
+#         logging.info('all_arguments: %s' % str(args))
+#         
+#         remove_rst = self.containerOpers.destory(args)
+#         if not remove_rst:
+#             raise HTTPAPIError(status_code=417, error_detail="container remove failed!",\
+#                                 notification = "direct", \
+#                                 log_message= "container remove failed!",\
+#                                 response =  "container remove failed!")
+#         
+#         dict = {}
+#         dict.setdefault("message", "remove container has been done but need some time, please wait a little and check the result!")
+#         
+#         self.finish(dict)
