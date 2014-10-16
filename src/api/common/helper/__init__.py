7563dd3b651842e968fd0b7baea47c32d6b82629
段炜1 <duanwei1@letv.com>
2014-10-16 10:06:48 +0800

    add init method use for container start: 1.change route immidiatly after container start: helper._init_container(containerName)

--- src/api/common/helper/__init__.py
+++ src/api/common/helper/__init__.py
@@ -60,3 +60,90 @@ def getDictFromText(sourceText, keyList):
             resultValue.setdefault(key,value)
             
     return resultValue
+
+def _init_container(containerName=None):
+    timeout = 5
+    get_route_cmd = r"route -n|grep -w 'UG'"
+    if containerName is None:
+        return False
+    try:
+        c = docker.Client('unix://var/run/docker.sock')
+        re_info = c.inspect_container(containerName)
+        for env in re_info['Config']['Env']:
+            if re.match(r"^IP=", env):
+                key, ip = re.split(r"=", env)
+            if re.match(r"^NETMASK=", env):
+                key, mask = re.split(r"=", env)
+        real_route = ''
+        for i in range(0,4):
+            if i != 3:
+                real_route = real_route + str(int(ip.split(r".")[i])&int(mask.split(r".")[i])) + r"."
+            else:
+                real_route = real_route + str((int(ip.split(r".")[i])&int(mask.split(r".")[i]))+1)
+        child = pexpect.spawn(r"docker attach %s" % (containerName))
+        child.expect(["bash", pexpect.EOF, pexpect.TIMEOUT], timeout=timeout)
+        child.sendline(get_route_cmd)
+        child.expect(['0.0.0.0.*bash', pexpect.EOF, pexpect.TIMEOUT], timeout=timeout)
+        if child.after == pexpect.TIMEOUT:
+            route_list = []
+        else:
+            route_list = child.after.replace('bash','').split("\r\n")
+        r_list = _get_route_dicts(route_list)
+        if r_list is False:
+            return False
+        if len(r_list) > 0:
+            for route in r_list:
+                if route['route_ip'] == real_route:
+                    continue
+                else:
+                    child.sendline(r"route del -net 0.0.0.0/%s gw %s dev %s" % (route['mask_num'], route['route_ip'], route['dev']))
+                    child.expect(["bash", pexpect.EOF, pexpect.TIMEOUT], timeout=timeout)
+        child.sendline(get_route_cmd)
+        child.expect(['0.0.0.0.*bash', pexpect.EOF, pexpect.TIMEOUT], timeout=timeout)
+        if child.after == pexpect.TIMEOUT:
+            route_list = []
+        else:
+            route_list = child.after.replace("bash",'').split("\r\n")
+        r_list = _get_route_dicts(route_list)
+        if r_list is False:
+            return False
+        if len(r_list) == 0:
+            child.sendline(r"route add default gw %s" % (real_route))
+            child.expect(["#", pexpect.EOF, pexpect.TIMEOUT], timeout=timeout)
+            child.sendline(r"")
+            child.close()
+        elif len(r_list) > 1 or r_list[0]['route_ip'] != real_route:
+            return False
+    except Exception, e:
+        logging.error(e)
+        raise e
+    return True
+
+def _get_route_dicts(route_list=None):
+    if route_list is None:
+        return False
+    r_list = []
+    for line in route_list:
+        if ( line == '' ): continue
+        route_info = {}
+        route_info['route_ip'] = line.split()[1]
+        mask_num = _mask_to_num(line.split()[2])
+        if type(mask_num) is dict:
+            logging.error('netmask Illegal: %s' % (mask_num['false']))
+            return False
+        else:
+            route_info['mask_num'] = mask_num
+        route_info['dev'] = line.split()[7]
+        r_list.append(route_info)
+    return r_list
+
+def _mask_to_num(netmask=None):
+    if netmask is None:
+        return { 'false': netmask }
+    num = ''
+    for i in range(0,4):
+        ip = int(netmask.split(r".")[i])
+        if ip > 255 or ip < 0:
+            return { 'false': netmask }
+        num = num + str(bin(ip).replace('0b',''))
+    return len(num.replace(r"0",''))
