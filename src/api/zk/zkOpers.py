5d20ea3749c2d98ca84af836637cb18781438a4a
zhangzeng <zhangzeng@letv.com>
2015-03-23 16:24:51 +0800

    commit @todo

--- src/api/zk/zkOpers.py
+++ src/api/zk/zkOpers.py
@@ -364,8 +364,8 @@ class ZkOpers(object):
         for port in rest_port_list:
             port_path = path + "/" + port
             self.zk.delete(port_path)
-#             if nc_ip_port_available(host_ip, port):
-#                 assign_port_list.append(port)
+            if not nc_ip_port_available(host_ip, port):
+                assign_port_list.append(port)
             assign_port_list.append(port)
             if len(assign_port_list) == port_count:
                 break
