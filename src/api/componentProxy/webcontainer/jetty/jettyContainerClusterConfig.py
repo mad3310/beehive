e3a9656a1d543652a6f0c07f66b9976b1a72c1e1
zhangzeng <zhangzeng@letv.com>
2015-03-24 18:19:11 +0800

    test 1

---
+++ src/api/componentProxy/webcontainer/jetty/jettyContainerClusterConfig.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+
+'''
+Created on 2015-3-9
+
+@author: asus
+'''
+
+from componentProxy.baseClusterConfig import BaseContainerClusterConfig
+
+class JettyContainerClusterConfig(BaseContainerClusterConfig):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params={}):
+        super(JettyContainerClusterConfig, self).__init__(params)
+            
+        nodeCount = params.get('nodeCount')                          
+        self.nodeCount = int(nodeCount) if nodeCount else 1
+        image = params.get('image')
+        self.image = image if image else 'letv/jetty:0.0.1'
+        ports = params.get('ports')
+        self.ports = eval(ports) if ports else [8080]
