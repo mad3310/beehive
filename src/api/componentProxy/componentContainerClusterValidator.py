54fe08d0b91ac57d4e2a82640c75d8f08c8173fd
zhangzeng <zhangzeng@letv.com>
2015-03-25 14:33:01 +0800

    test 1

--- src/api/componentProxy/componentContainerClusterValidator.py
+++ src/api/componentProxy/componentContainerClusterValidator.py
@@ -4,11 +4,8 @@ Created on 2015-2-4
 @author: asus
 '''
 import logging
+from zk.zkOpers import ZkOpers
 from status.status_enum import Status
-from componentProxy.db.mysql.mysqlContainerClusterValidator import MysqlContainerClusterValidator
-from componentProxy.loadbalance.gbalancer.gbalancerContainerClusterValidator import GbalancerContainerClusterValidator
-from componentProxy.webcontainer.nginx.nginxContainerClusterValidator import NginxContainerClusterValidator
-from componentProxy.webcontainer.jetty.jettyContainerClusterValidator import JettyContainerClusterValidator
 
 
 class ComponentContainerClusterValidator(object):
@@ -16,27 +13,12 @@ class ComponentContainerClusterValidator(object):
     classdocs
     '''
     
+    zk_oper = ZkOpers()
 
     def __init__(self):
         '''
         Constructor
         '''
-        
-#     def container_cluster_status_validator(self, _component_type, container_cluster_name):
-#         _check_result = False
-#         if "mclusternode" == _component_type:
-#             container_cluster_validator = MysqlContainerClusterValidator()
-#         elif "mclustervip" == _component_type:
-#             container_cluster_validator = GbalancerContainerClusterValidator()
-#         elif 'nginx' == _component_type:
-#             container_cluster_validator = NginxContainerClusterValidator()
-#         elif 'jetty' == _component_type:
-#             container_cluster_validator = JettyContainerClusterValidator()
-#         else:
-#             container_cluster_validator = None
-#             
-#         _check_result = container_cluster_validator.validate_container_cluster_status(container_cluster_name)
-#         return _check_result
 
     def container_cluster_status_validator(self, _component_type, cluster):
         
