9f0481e77476097904c954cfff5074cb190ae0ad
zhangzeng <zhangzeng@letv.com>
2015-04-27 15:33:13 +0800

    cbase access

--- src/api/componentProxy/store/cbase/cbaseContainerClusterConfig.py
+++ src/api/componentProxy/store/cbase/cbaseContainerClusterConfig.py
@@ -19,9 +19,9 @@ class CbaseContainerClusterConfig(BaseContainerClusterConfig):
         nodeCount = params.get('nodeCount')
         self.nodeCount = int(nodeCount) if nodeCount else 3
         image = params.get('image')
-        self.image = image if image else '10.160.140.32:5000/lihanlin1/cbase:V2'
+        self.image = image if image else '10.160.140.32:5000/lihanlin1/cbase:V3'
         
-        default_mount_dir =  [{'/srv':'/srv/tmp/c1/'}]
+        default_mount_dir =  [{'/ookk':'/host/ookk', 'ro':False}]
         
         mount_dir = params.get('mountDir')
         self.mount_dir = eval(mount_dir) if mount_dir else default_mount_dir
