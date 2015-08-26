7c7775a3e71e512f34ecab5f47523eb0c5f65473
unknown <mazheng@CNBJWKDBX15Z1.letv.local>
2015-08-26 18:20:59 +0800

    add manager status method

--- src/api/componentProxy/distribution/zookeeper/zookeeperOper.py
+++ src/api/componentProxy/distribution/zookeeper/zookeeperOper.py
@@ -7,3 +7,6 @@ class ZookeeperManager(BaseComponentManager):
 
     def __init__(self):
         super(ZookeeperManager,self).__init__('zookeeper-manager')
+
+    def manager_status(self, container_name = None):
+        return True
