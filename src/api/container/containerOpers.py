3686cc490f92d11ed9a048fd7f2ddd7805d8df1f
zhangzeng <zhangzeng@letv.com>
2015-09-08 16:45:34 +0800

    test

--- src/api/container/containerOpers.py
+++ src/api/container/containerOpers.py
@@ -558,11 +558,10 @@ class Container_create_action(Abstract_Async_Thread):
     def _get_container_info(self):
         _container_name = self.docker_model.name
         _type = self.docker_model.component_type
-        _host_ip = self.docker_model.host_ip
         if has_property(self.docker_model, 'added'):
             _added = True
         
-        return self.container_opers.container_info(_container_name, _type, _host_ip, _added)
+        return self.container_opers.container_info(_container_name, _type, _added)
 
     def __get_route_dicts(self, route_list=None):
         if route_list is None:
