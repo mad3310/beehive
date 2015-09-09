6b51f0150e2bdce61150c293fd5cddc2faddf85c
zhangzeng <zhangzeng@letv.com>
2015-09-09 16:24:16 +0800

    add cbase  validator

--- src/api/componentProxy/store/cbase/cbaseOper.py
+++ src/api/componentProxy/store/cbase/cbaseOper.py
@@ -1,15 +1,17 @@
 #-*- coding: utf-8 -*-
 
 import pexpect
-import time
 
 from componentProxy.baseComponentOpers import BaseComponentManager
 
 
-class MclusterManager(BaseComponentManager):
+class CbaseManager(BaseComponentManager):
 
     def __init__(self):
         self.timeout = 5
+
+#     def manager_status(self, container_name = None):
+#         return True
 
     def manager_status(self, container_name = None):
         if container_name is None:
@@ -22,11 +24,9 @@ class MclusterManager(BaseComponentManager):
         child = pexpect.spawn(r"docker attach %s" % container_name)
         
         try:
-            child.expect(["bash", pexpect.EOF, pexpect.TIMEOUT], timeout=self.timeout)
-            child.sendline("service %s restart" % self.component_manager)
-            child.expect(["OK", pexpect.EOF, pexpect.TIMEOUT], timeout=self.timeout)
-
-            time.sleep(1)
+            child.sendline("service cbase restart")
+            child.expect(["OK", pexpect.EOF, pexpect.TIMEOUT], timeout=10)
+            
             child.sendline("service cbase status")
             index = child.expect(["running", pexpect.EOF, pexpect.TIMEOUT], timeout=self.timeout)
             if index != 0:
