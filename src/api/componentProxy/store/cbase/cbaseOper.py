afd540bef8aa32481f228568190e0c6824cf6a77
zhangzeng <zhangzeng@letv.com>
2015-09-24 17:52:57 +0800

    test cbase

--- src/api/componentProxy/store/cbase/cbaseOper.py
+++ src/api/componentProxy/store/cbase/cbaseOper.py
@@ -10,25 +10,25 @@ from componentProxy.baseComponentOpers import BaseComponentManager
 class CbaseManager(BaseComponentManager):
 
     def __init__(self):
-        self.timeout = 5
+        super(CbaseManager, self).__init__('cbase', "service cbase status'", 'running')
 
-    def manager_status(self, container_name):
-        command = "docker ps | grep %s | awk '{print $1}'" % container_name 
-        ret = os.popen(command)
-        container_id = ret.read().strip()
-        return self.__check(container_id)
-
-    def __check(self, container_id = None):
-        
-        stat = True
-        nsenter = options.nsenter % container_id
-        
-        cbase_status_cmd = nsenter + ' service cbase status'
-        ret = os.popen(cbase_status_cmd)
-        status = ret.read()
-        if not Flag.running in status:
-            stat = False
-            cbase_restart_cmd = nsenter + ' service cbase restart'
-            os.system(cbase_restart_cmd)
-        
-        return stat
+#     def manager_status(self, container_name):
+#         command = "docker ps | grep %s | awk '{print $1}'" % container_name 
+#         ret = os.popen(command)
+#         container_id = ret.read().strip()
+#         return self.__check(container_id)
+# 
+#     def __check(self, container_id = None):
+#         
+#         stat = True
+#         nsenter = options.nsenter % container_id
+#         
+#         cbase_status_cmd = nsenter + ' service cbase status'
+#         ret = os.popen(cbase_status_cmd)
+#         status = ret.read()
+#         if not Flag.running in status:
+#             stat = False
+#             cbase_restart_cmd = nsenter + ' service cbase restart'
+#             os.system(cbase_restart_cmd)
+#         
+#         return stat
