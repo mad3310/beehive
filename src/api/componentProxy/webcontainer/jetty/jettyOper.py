5a025467ee78d046c8ae62ee52d490c484d139ac
ÕÅÔö <zhangzeng@letv.com>
2015-12-31 16:23:07 +0800

    commit local

--- src/api/componentProxy/webcontainer/jetty/jettyOper.py
+++ src/api/componentProxy/webcontainer/jetty/jettyOper.py
@@ -7,6 +7,3 @@ class JettyManager(BaseComponentManager):
 
     def __init__(self):
         super(JettyManager, self).__init__('jetty-manager')
-
-     def manager_status(self, container_name = None):
-         return True
