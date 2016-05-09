417490d5f7e6098919d8bb6f6d41d9c0b3fb0841
邓良驹 <dengliangju@le.com>
2016-05-09 09:01:27 +0200

    增加rpm打包相关文件

---
+++ scripts/rpm/after_remove.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /opt/letv/container-manager
+rm -rf /etc/init.d/container-manager
+rm -rf /etc/sysconfig/container-manager
+
+exit 0
---
+++ scripts/rpm/before_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /opt/letv
+mkdir -p /var/log/container-manager
+
+exit 0
