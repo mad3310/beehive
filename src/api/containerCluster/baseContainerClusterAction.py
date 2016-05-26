c52be34ea4ca2f46aca62c6455b72ca2a89749b2
denglj <dengliangju@le.com>
2016-05-26 15:50:57 +0800

    代码PEP8格式化

--- src/api/containerCluster/baseContainerClusterAction.py
+++ src/api/containerCluster/baseContainerClusterAction.py
@@ -23,7 +23,7 @@ from container.containerOpers import Container_Opers
 
 class Base_ContainerCluster_Action(Abstract_Async_Thread):
     """if param "containers" not given, the action is about cluster;
-     
+
     if param "containers" are given, the action is about such containers.
     """
 
@@ -45,7 +45,7 @@ class Base_ContainerCluster_Action(Abstract_Async_Thread):
         params = self.__get_params()
         adminUser, adminPasswd = _retrieve_userName_passwd()
         logging.info('params: %s' % str(params))
-        
+
         async_client = AsyncHTTPClient()
         try:
             for host_ip, container_name_list in params.items():
@@ -57,7 +57,7 @@ class Base_ContainerCluster_Action(Abstract_Async_Thread):
                     async_http_post(async_client, request_uri, body=args, auth_username=adminUser, auth_password=adminPasswd)
         finally:
             async_client.close()
-        
+
         if self.action == 'remove':
             self.do_when_remove_cluster()
 
@@ -74,9 +74,9 @@ class Base_ContainerCluster_Action(Abstract_Async_Thread):
         """
             two containers may be with a host_ip
         """
-        
+
         params, container_info, container_nodes = {}, {}, []
-        
+
         zkOper = Container_ZkOpers()
         if self.containers:
             for container in self.containers:
@@ -98,15 +98,15 @@ class Base_ContainerCluster_Action(Abstract_Async_Thread):
 
 
 class Base_ContainerCluster_create_Action(Abstract_Async_Thread):
-    
+
     resource = Resource()
-    
+
     component_manager_status_validator = ComponentManagerStatusValidator()
-    
+
     component_container_cluster_validator = ComponentContainerClusterValidator()
-    
+
     base_container_model_creator = BaseContainerModelCreator()
-    
+
     def __init__(self, arg_dict={}):
         super(Base_ContainerCluster_create_Action, self).__init__()
         self._arg_dict = arg_dict
@@ -116,54 +116,54 @@ class Base_ContainerCluster_create_Action(Abstract_Async_Thread):
 
     def create(self, args={}):
         logging.info('args:%s' % str(args))
-        
+
         _component_type = args.get('componentType')
         cluster = args.get('containerClusterName')
         logging.info('component_type : %s' % str(_component_type))
         logging.info('containerClusterName : %s' % cluster)
-        
+
         _component_container_cluster_config = args.get('component_config')
-        
+
         """
-            ---------------------------------  resource validate ---------------------------------------------  
+            ---------------------------------  resource validate ---------------------------------------------
         """
 
         is_res_verify = _component_container_cluster_config.is_res_verify
         if is_res_verify:
             self.resource.validateResource(_component_container_cluster_config)
-        
+
         host_ip_list = self.resource.elect_servers(_component_container_cluster_config)
-        
+
         logging.info('host_ip_list:%s' % str(host_ip_list))
         args.setdefault('host_ip_list', host_ip_list)
-        
+
         NIC = self.retrieve_cluster_NIC()
         args.setdefault('NIC', NIC)
-        
+
         ip_port_resource_list = self.resource.retrieve_ip_port_resource(host_ip_list, _component_container_cluster_config)
         args.setdefault('ip_port_resource_list', ip_port_resource_list)
 
         """
-            ---------------------------------  get create container params--------------------------------------  
+            ---------------------------------  get create container params--------------------------------------
         """
 
         logging.info('show args to get create containers args list: %s' % str(args))
         container_model_list = self.base_container_model_creator.create(args)
 
         """
-            ---------------------------- dispatch creating task and check the result----------------------------  
+            ---------------------------- dispatch creating task and check the result----------------------------
         """
 
         self.__dispatch_create_container_task(container_model_list)
-        
+
         created = self.__check_containers_started(_component_container_cluster_config)
         if not created:
             raise CommonException('cluster started failed, maybe part of nodes started, other failed!')
-        
+
         _action_flag = True
         if _component_container_cluster_config.need_validate_manager_status:
             _action_flag = self.component_manager_status_validator.validate_manager_status_for_cluster(_component_type, container_model_list)
-        
+
         logging.info('validator manager status result:%s' % str(_action_flag))
         _action_result = Status.failed if not _action_flag else Status.succeed
         return _action_result
@@ -180,23 +180,23 @@ class Base_ContainerCluster_create_Action(Abstract_Async_Thread):
         return NIC
 
     def __dispatch_create_container_task(self, container_model_list):
-        
+
         ip_port_params_list = []
         for container_model in container_model_list:
             property_dict = _get_property_dict(container_model)
             host_ip = property_dict.get('host_ip')
             ip_port_params_list.append((host_ip, options.port, property_dict))
-        
+
         dispatch_mutil_task(ip_port_params_list, '/inner/container', 'POST')
 
     def __check_containers_started(self, component_container_cluster_config):
-        
+
         container_cluster_name = component_container_cluster_config.container_cluster_name
         sum_count = component_container_cluster_config.sum_count
         return handleTimeout(self.__is_containers_started, (250, 4), container_cluster_name, sum_count)
 
     def __is_containers_started(self, container_cluster_name, sum_count):
-        
+
         zkOper = Container_ZkOpers()
         container_list = zkOper.retrieve_container_list(container_cluster_name)
         if len(container_list) != sum_count:
@@ -208,7 +208,7 @@ class Base_ContainerCluster_create_Action(Abstract_Async_Thread):
         return status.get('status') == Status.started
 
     def update_zk_info_when_process_complete(self, cluster, create_result='failed', error_msg=''):
-        
+
         zkOper = Container_ZkOpers()
         _container_cluster_info = zkOper.retrieve_container_cluster_info(cluster)
         _container_cluster_info['start_flag']=create_result
