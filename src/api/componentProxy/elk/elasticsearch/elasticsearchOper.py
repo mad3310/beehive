0f596dd324977ed18c15a1e184633e86e2b19789
liujinliu <liujinliu@le.com>
2016-07-25 15:15:05 +0800

    check if logs-manager starting

--- src/api/componentProxy/elk/elasticsearch/elasticsearchOper.py
+++ src/api/componentProxy/elk/elasticsearch/elasticsearchOper.py
@@ -2,12 +2,40 @@ __author__ = 'xsank'
 
 
 from componentProxy.baseComponentOpers import BaseComponentManager
-
+import os
+import time
+import logging
+from tornado.options import options
 
 class ElasticsearchManager(BaseComponentManager):
 
     def __init__(self):
         super(ElasticsearchManager, self).__init__('logs-manager')
 
-    def manager_status(self, container_name = None):
-        return True
+    def manager_status(self, container_name):
+        command = "docker ps | grep %s | awk '{print $1}'" \
+                  % container_name 
+        ret = os.popen(command)
+        container_id = ret.read().strip()
+        logging.info('container id :%s, command:%s'
+                    % (container_id, command))
+        return self.__check(container_id)
+
+    def __check(self, container_id):
+        result = True
+        time.sleep(4)
+        nsenter = options.nsenter % container_id
+        cmd = self.validate_cmd if self.validate_cmd else \
+              "curl -d '' 'http://127.0.0.1:9999/admin/conf'"
+        curl_cmd = nsenter + cmd
+        logging.info('curl check:%s' % curl_cmd)
+        ret = os.popen(curl_cmd)
+        status = ret.read()
+        logging.info('ns check result :%s' % status)
+        flag = self.validate_flag if self.validate_flag else 'successful'
+        if not flag in status:
+            result = False
+            component_restart_cmd = nsenter + \
+                'service %s restart' % self.component_manager
+            os.system(component_restart_cmd)
+        return result
